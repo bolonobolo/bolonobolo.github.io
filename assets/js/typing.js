2e7f9b75331c23b61e63abc2523c90c3f20d602a
bolo <bolo@autistici.org>
2024-11-05 11:46:51 +0100

    modify colors

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,15 +1,9 @@
 const commands = [
-    "cdq  ; xor edx",
-    "mul edx",
-	"lea ecx, [eax]",
-	"mov esi, 0x68732f2f",
-	"mov edi, 0x6e69622f",
-	"push ecx                ; push NULL in stack",
-	"push esi",
-	"push edi                ; push hs/nib// in stack",
-	"lea ebx, [esp]          ; load stack pointer to ebx",
-	"mov al, 0xb             ; load execve in eax",
-	"int 0x80"
+    "./share_the_knowledge",
+    "./exploit_breaking_stuffs",
+	"sh community && ./be_safe",
+	"./run --privacy --community",
+	"./reuse && ./recycle"
   ];
   
   let commandIndex = 0;
